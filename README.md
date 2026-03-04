10b8a4f52ed87e0800047b54e7670052754ba99b
SiKaminski <simonkaminski1100@gmail.com>
2026-03-03 18:19:21 -0700

    Simple build system with SDL linked

---
+++ README.md
@@ -0,0 +1,7 @@
+CPP Image Viewer
+
+## Dependencies
+
+- sdl3
+- cmake
+- gcc
---
+++ src/main.cpp
@@ -0,0 +1,32 @@
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_error.h>
+#include <SDL3/SDL_events.h>
+#include <SDL3/SDL_video.h>
+#include <iostream>
+
+int main() {
+    if (SDL_Init(SDL_INIT_VIDEO)) { 
+        std::cerr << "SDL could not initialize " << SDL_GetError() << std::endl;
+        return 1;
+    }
+
+    SDL_Window* window = SDL_CreateWindow(
+            "Hello World",
+            800,
+            600,
+            SDL_WINDOW_RESIZABLE
+    );
+
+    if (!window) {
+        std::cerr << "Window could not be created " << SDL_GetError() << std::endl;
+        SDL_Quit();
+        return 1;
+    }
+
+    SDL_Delay(3000);
+
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    return 0;
+}
