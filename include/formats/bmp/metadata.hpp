d32619329da6cc0783045dca0535ed2ba580c9a4
SiKaminski <simonkaminski1100@gmail.com>
2026-03-22 22:16:37 -0700

    bmp InfoHeader

--- include/formats/bmp/metadata.hpp
+++ include/formats/bmp/metadata.hpp
@@ -10,12 +10,28 @@ namespace Format
     namespace BMP
     {
         constexpr int HEADER_BYTE_LEN {14};
+        constexpr int INFO_HEADER_BYTE_LEN {40};
 
         struct FileHeader {
             byte Signature[2];
             byte FileSize[4];
             byte RESV[4];
             byte FileOffset[4];
+        } __attribute__((packed));
+
+        struct InfoHeader {
+            byte Size[4];
+            byte Width[4];
+            byte Height[4];
+            byte Planes[2];
+            byte BitCount[2];
+            byte Compression [4];
+            byte ImageSize[4];
+            byte XpixelsPerM[4];
+            byte YpixelsPerM[4];
+            byte ColorsUsed[4];
+            byte ColorsImportant[4];
+            byte* ColorTable; // 4 * NumColors only if BitsPerPixel<=8
         } __attribute__((packed));
     }
 }
